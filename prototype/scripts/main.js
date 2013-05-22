5fa2c99b5215aeb63919c0c47643fe13923caba9
Kyle Florence <kflorence@wikia-inc.com>
2013-05-22 14:19:56 -0700

    Update prototype 1.

--- prototype/scripts/main.js
+++ prototype/scripts/main.js
@@ -1,12 +1,24 @@
 jQuery(function( $ ) {
+	var $window = $( window );
+	var $stickyNav = $( '#Header .ribbon-wrapper' );
+	var $topToolbar = $( '#ToolbarTop' );
+	var topToolbarHeight = $topToolbar.height();
+
 	var grid = new hashgrid({
 		foregroundKey: 'f',
 		id: 'Overlay',
 		showGridKey: 'g'
 	});
 
-	var $wrapper = $( '#ContentWrapper' ).on( 'click', '.toggle span', function( event ) {
-		$wrapper.toggleClass( 'narrow wide' );
-		$( event.target ).html( $wrapper.hasClass( 'wide' ) ? '&gt;' : '&lt;' );
+	// TODO: when implementing this for real, we should probably have some kind of
+	// algorithm that throttles the scroll callback based on distance from sticky
+	// point and how fast the user is scrolling to prevent eating up CPU cycles.
+	$window.on( 'scroll.stickyNav', function() {
+		var stickyNavOffsetTop = $stickyNav.data( 'stickyNavOffsetTop' ) || $stickyNav.offset().top,
+			stick = ( $window.scrollTop() + topToolbarHeight ) >= stickyNavOffsetTop;
+
+		$stickyNav
+			.toggleClass( 'stuck', stick )
+			.data( 'stickyNavOffsetTop', stick && stickyNavOffsetTop );
 	});
 });
